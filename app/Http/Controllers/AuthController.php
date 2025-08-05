dd6e23cae173f31e6807faf332029699de79b71b
Anthony Pablo <apablo_dev@pisopay.com.ph>
2025-08-05 11:04:38 +0800

    Implement jwt

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,12 +11,19 @@ class AuthController extends Controller
     {
         $credentials = $request->only('email', 'password');
 
-        if (!$token = Auth::attempt($credentials)) {
+        if (! $token = Auth::attempt($credentials)) {
             return response()->json(['error' => 'Unauthorized'], 401);
         }
 
+        return $this->respondWithToken($token);
+    }
+
+    protected function respondWithToken($token)
+    {
         return response()->json([
             'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60,
             'user' => Auth::user()
         ]);
     }
